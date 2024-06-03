34d51b4d29cd1f24ecbbd9d9f3d83564847319a1
balajich004 <balajichennupati123@gmail.com>
2024-06-03 23:46:44 +0530

    completed the popup using a new dialog tag

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
+const library=document.querySelector(".library");
+const add=document.querySelector(".addbook");
+const dialog=document.querySelector("dialog");
+const close=document.querySelector("#close");
 const myLibrary=[];
-function Book(){}
+function Book(title,author,pages,read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+}
 function addToLibrary(){}
 function display(){
     myLibrary.map((book)=>{
-        
+        library.add
     });
 }
+
+add.addEventListener("click", () => {
+    dialog.showModal();
+    // dialog.style.top=15;
+  });
+close.addEventListener("click",()=>{
+    dialog.close();
+  });
