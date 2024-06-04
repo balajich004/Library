4cc1f2fc71a6172ff7216b873ac58d45d3535d51
balajich004 <balajichennupati123@gmail.com>
2024-06-04 13:44:43 +0530

    completed the basic working of adding books to library

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,13 @@ const library=document.querySelector(".library");
 const add=document.querySelector(".addbook");
 const dialog=document.querySelector("dialog");
 const close=document.querySelector("#close");
-const myLibrary=[];
-function Book(title,author,pages,read){
+const btitle=document.querySelector("#title");
+const bauthor=document.querySelector("#author");
+const selectedRadio = document.querySelector('input[type="radio"]');
+const submit=document.querySelector("#submit");
+const bpages=document.querySelector("#pages");
+let myLibrary=[];
+function Books(title,author,pages,read){
     this.title=title;
     this.author=author;
     this.pages=pages;
@@ -12,13 +17,24 @@ function Book(title,author,pages,read){
 function addToLibrary(){}
 function display(){
     myLibrary.map((book)=>{
-        library.add
+        
     });
 }
-
+submit.addEventListener("click",()=>{
+    let title=btitle.value;
+    let author=bauthor.value;
+    let pages=bpages.value;
+    let selectedValue = selectedRadio.value;
+    if (selectedValue==="mread") {
+        selectedValue="Marked as Read";
+    } else {
+        selectedValue="Marked as Not Read";
+    }
+    myLibrary.push(new Books(title,author,pages,selectedValue));
+    console.log(myLibrary);
+});
 add.addEventListener("click", () => {
     dialog.showModal();
-    // dialog.style.top=15;
   });
 close.addEventListener("click",()=>{
     dialog.close();
